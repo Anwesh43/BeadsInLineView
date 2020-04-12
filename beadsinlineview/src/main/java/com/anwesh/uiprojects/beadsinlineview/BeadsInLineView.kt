a5c3ef0565ec5a3d623d7619633cb0681e2d5f91
Anwesh43 <anweshthecool0@gmail.com>
2020-04-13 04:17:52 +0530

    adding Canvas extension methods

--- beadsinlineview/src/main/java/com/anwesh/uiprojects/beadsinlineview/BeadsInLineView.kt
+++ beadsinlineview/src/main/java/com/anwesh/uiprojects/beadsinlineview/BeadsInLineView.kt
@@ -15,13 +15,40 @@ import android.app.Activity
 val nodes : Int = 5
 val beads : Int = 3
 val scGap : Float = 0.02f
-val strokeFactor : Int = 90
 val sizeFactor : Float = 2.9f
 val foreColor : Int = Color.parseColor("#3F51B5")
 val backColor : Int = Color.parseColor("#BDBDBD")
 val delay : Long = 20
+val offsetFactor : Float = 4f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawBeadInLine(i : Int, w : Float, scale : Float, paint : Paint) {
+    val offset : Float = w / offsetFactor
+    val gap : Float = offset / beads
+    val sc : Float = scale.sinify().divideScale(i, beads)
+    save()
+    translate(gap / 2 + (beads - 1 - i) * gap + (w - offsetFactor) * sc, 0f)
+    drawCircle(0f, 0f, gap / 2, paint)
+    restore()
+}
+
+fun Canvas.drawBeadsInLine(w : Float, scale : Float, paint : Paint) {
+    for (j in 0..(beads - 1)) {
+        drawBeadInLine(j, w, scale, paint)
+    }
+}
+
+fun Canvas.drawBILNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    paint.color = foreColor
+    save()
+    translate(0f, gap * (i + 1))
+    drawBeadsInLine(w, scale, paint)
+    restore()
+}
