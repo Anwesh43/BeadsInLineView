051078fcd7d1d9c299e0913b5ccf381a0d06b937
Anwesh43 <anweshthecool0@gmail.com>
2020-04-13 04:25:31 +0530

    adding BILNode class

--- beadsinlineview/src/main/java/com/anwesh/uiprojects/beadsinlineview/BeadsInLineView.kt
+++ beadsinlineview/src/main/java/com/anwesh/uiprojects/beadsinlineview/BeadsInLineView.kt
@@ -114,4 +114,46 @@ class BeadsInLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BILNode(var i : Int, val state : State = State()) {
+
+        private var next : BILNode? = null
+        private var prev : BILNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = BILNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBILNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BILNode {
+            var curr : BILNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
