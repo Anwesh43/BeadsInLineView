e63b64f2b750ae6319fb9eff70bc4303a48a7b75
Anwesh43 <anweshthecool0@gmail.com>
2020-04-13 03:59:34 +0530

    adding constants and libraries

--- beadsinlineview/src/main/java/com/anwesh/uiprojects/beadsinlineview/BeadsInLineView.kt
+++ beadsinlineview/src/main/java/com/anwesh/uiprojects/beadsinlineview/BeadsInLineView.kt
@@ -4,3 +4,19 @@ package com.anwesh.uiprojects.beadsinlineview
  * Created by anweshmishra on 13/04/20.
  */
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.content.Context
+import android.app.Activity
+
+val nodes : Int = 5
+val beads : Int = 3
+val scGap : Float = 0.02f
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#3F51B5")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
